b309723a2d6ecba521c954e8f6456f33b421013c
Alex Preiss <alexspreiss@gmail.com>
2019-08-30 14:46:08 -0700

    fix: trialing slash redirect

--- app.js
+++ app.js
@@ -4,12 +4,7 @@ const path = require("path")
 const app = express()
 const port = 3000
 
-app.use(
-  express.static(path.join(__dirname, "public"), {
-    maxAge: 864000000, // 10 days in milliseconds
-    redirect: false,
-  })
-)
+app.use(express.static(path.join(__dirname, "public")))
 
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "public/404/index.html"))
